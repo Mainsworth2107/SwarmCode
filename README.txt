b17dad3365daf19a9bebd2c9f59b5d5390d2c85b
Matthew Ainsworth <mattainsworth1@gmail.com>
2020-11-12 18:09:36 +0000

    Neat write up

--- README.txt
+++ README.txt
@@ -2,9 +2,11 @@ README for DBA code
 
 DBA Algorithm (Andina et al): Implementation and verification of DBA Algorithm
 
-FLIES:
+FILES:
 
-distEU: Calculates the euclidian distance between any equal sized point sets (A) and (B).
+DBAFit.m: Uses the method described in Andina et al to produce an allocation for each robot in the swarm.
+
+distEU: Calculates the Euclidian distance between any equal sized point sets (A) and (B).
 
 extPoses.m Extracts the pose information from each robot to allow for the environment object to plot them.
 
@@ -15,8 +17,6 @@ height.m Returns the height of a 2d matrix.
 initBots.m Initialises a random robot placement to mimic a single allocation situation within the simulation.
  
 main.m main code run for simulation. Calls all other functions in its process.
-
-newfit.m: Uses the method described in Andina et al to produce an allocation for each robot in the swarm.
 
 robot.m Class file for each robot that contains the FSM code used for iterative running.
 
--- main.m
+++ main.m
@@ -1,166 +1,232 @@
-%% Distributed bees aglrotihm
-%% Based on:
-%% Uses The Mobile Simulation Toolbox from: The MathWorks, Inc.
-
-
+%% Distributed bees algorithm
+ 
+%% Acknowledgements
+ 
+% Based on: Distributed Bees Algorithm for Task Allocation in Swarm of Robots
+% Jevtić et al, 2012
+ 
+% Uses The Mobile Robotics Simulation Toolbox (MRST) from: The MathWorks, Inc.
+% Note: A small modification was made to the MRST code to draw the objects
+% in the environment as slightly larger than the default size.
+ 
+%N represents the number of robots (in comments)
+%M represents the number of objects (in comments)
+ 
 %% Create a multi-robot environment
 flush
-numRobots = 10; %Self explanitary
-
-env = MultiRobotEnv(numRobots); %Initalises robot envrionment
-env.showTrajectory = false; %Disables live pathing
-% env.robotRadius = 0.25; %Determines robot chunkiness
-
-limX = [-14 14]; % 8
+numRobots = 40; %Initialises the number of robots.
+ 
+env = MultiRobotEnv(numRobots); %Initialises robot envrionment (MRST)
+env.showTrajectory = false; %Disables robot pathing 
+ 
+% Limits of visualisation axes
+limX = [-14 14]; 
 limY = [-10, 10];
-
-A = zeros(1,numRobots); %Allocation
-
-%% Initalising Objects
-
-% setup 1
-objs = 2;
-qualities = [0.5,0.5];
-
-%setup 2
+ 
+A = zeros(1,numRobots); %Calculated allocation
+ 
+%% Selecting testing scenario
+ 
+% Setup 1
+objs = 2;                       % Number of Targets
+qualities = [0.5,0.5];          % Target qualities (q)
+preset = [-4.5,7.5; 4.5,-7.5];  % Object positions
+ 
+% Setup 2
 % objs = 2;
 % qualities = 0.25*ones(1,4);
-
-%setup 3
+% preset = [-4.5,7.5; 4.5,-7.5;-4.5,-7.5; 4.5,7.5];
+ 
+% Setup 3
 % objs = 2;
 % qualities = [0.1,0.2,0.3,0.4];
-
+% preset = [-4.5,7.5; 4.5,-7.5;-4.5,-7.5; 4.5,7.5];
+ 
 %% Adding Objects to environment
-objects = zeros(objs,3);
-colours = objects;
-
-%Stores the detection data of an object.
-detected = [[],[]];
-%preset = [2,2;2,14;14,2;14,14]; %Must be the same size as all objects
-preset = [-4.5,7.5; 4.5,-7.5];% ;-4.5,-7.5; 4.5,7.5]; %];
-
-objstr = 'o';
-for i = 1:objs
+objects = zeros(objs,3); %Stores the coordinates of each object
+colours = objects; %Sets the colour of each object
+ 
+% Note the objects array also stores the object ID to account for situations
+% where strict object ordering might not be possible or necessary.
+ 
+objstr = 'o'; %Marker to draw the objects as circles.
+ 
+for i = 1:objs %Initialises the objects within the environment
     objects(i,3) = i;
+    
+    %Positions are preset for the DBA
     objects(i,1:2) = preset(i,:);
-    %objects(i,1:2) = [rand*limX(2),rand*limY(2)];
-
-    % Sets the necessary labels to draw all objects as circles
+    %objects(i,1:2) = [rand*limX(2),rand*limY(2)]; %Random object initialisation
+ 
+    %Sets the necessary labels to draw all objects as circles
     if( i > 1)
         objstr = strcat(objstr,'o');
     end  
+    
     %Makes all objects red
     colours(i,1) = 1;
     colours(i,2:3) = 0;
 end
-
-%Initalises global objects reference
+ 
+%Initialises global objects reference
 global allObjs;
 allObjs = [objects,(qualities')];
-
+ 
+%Sets the necessary environment variables to draw the objects
 env.hasObjects = 1;
 env.objectColors = colours;
 env.objectMarkers = objstr;
-
+ 
 % Hides robot IDs
 env.showRobotIds = false;
-
-%% Initalises robots and arena
+ 
+%% Initialises robots and arena
 robots = cell(numRobots,1);
-
-% Arena dimensions
+ 
+%Arena dimensions
 diffX = 21.25;
 diffY = 15;
-
-robots = initBots(robots,objs,diffX,diffY);
-
-%% Setting up the visulisation
-
-%Initalising main envrionment visu
+ 
+%Function to initialise all robots
+robots = initBots(robots,objs,diffX,diffY); 
+ 
+% Allows for a sample robot position set to be loaded for specific tests
+% save('robots.mat','robots'); 
+% robots = load('robots.mat');
+% robots = robots.robots;
+ 
+%% Setting up the visualisation
+ 
+%Extracts the robot poses
 poses = extPoses(robots);
 env.Poses =  poses;
+ 
+%Draws the multi robot environment (this is an expensive operation, so not ran in loop). 
 env(1:numRobots, poses, objects);
-xlim(limX);   % Without this, axis resizing can slow things down
+ 
+% Ensure that the visualisations axes remain fixed. 
+% Without this, axis resizing can slow things down
+ 
+xlim(limX);   
 ylim(limY);  
-
-% Sets ojbect labels
+ 
+% Sets object labels
 for i = 1:objs
     text(objects(i,1) - 0.05,objects(i,2) - 0.3,num2str(i),'Color',[0,0,0],'FontWeight','bold');
 end
-
-%% Running the algortihm
-runs = 50;
-mae = zeros(1,runs);
+ 
+%% Running the algorithm
+runs = 50; %Total scenarios (robot position sets) to be tested
+ 
+%Initialises output variables
+mae = zeros(1,runs); % Mean absolute error.
 dists = mae;
 for i = 1:runs
-
-    counts = zeros(1,objs);
+ 
+    counts = zeros(1,objs); %Counts of each allocation for calculating MAE
+    
+    % For Each robot
     for j = 1:numRobots
         idx = (j + (numRobots*(i-1)));
-        if j <= objs
+        
+        if j <= objs % The first M robots will be allocated to their respective objects
             A(i,j) = j;
             counts(A(i,j)) = counts(A(i,j)) + 1;
         else
-            [P,Q] = newFit(robots{j}.pose);
-            % MAE does not consider already allocated robots.
-            A(i,j) = P;
+            
+            % Finds the allocation using the DBA fitness function, also
+            % returning distance
+            [A(i,j),Q] = DBAFit(robots{j}.pose); 
+            
+            %The distance does not need to consider the robots at objects
+            %As they are always 0 units from their respective goals.
             dists(i) = dists(i) + Q;
+            
+            % Adding 1 on each count ensure the robots on objects are
+            % considered
             counts(A(i,j)) = counts(A(i,j)) + 1;
         end
     end
+    % Normalises the robot counts
     counts = counts ./ (sum(counts));
+    
+    %Finds the mean absolute error between the actual robot distribution and
+    %the expected robot distribution
     mae(i) = (1/objs) * sum(abs(qualities - counts)); 
-    robots = initBots(robots,objs,diffX,diffY);
+    
+    %If not the final run, initialises a new robot position set (to test for consistency)
+    if (i < runs)
+        robots = initBots(robots,objs,diffX,diffY);
+    end
 end
-%% Producing an example visulisation
-
-sample = A(ceil(rand*height(A)),:);
+%% Producing an example visualisation
+%Example allocation chosen as last in set.
+sample = A(end,:); 
 poses = extPoses(robots);
-
+ 
+%Draws lines between each robot and its respective line to visually
+%represent the example allocation.
 for i = objs:numRobots
     line([robots{i}.pose(1),objects(sample(i),1)],...
          [robots{i}.pose(2),objects(sample(i),2)],...
          'color','black','LineWidth',1);
 end
+ 
+%Draw the example robot positions.
 env.Poses =  poses;
 env(1:numRobots, poses, objects);
-
-
-%% Bar Chart
-
+%% Bar chart comparing average allocation across all runs to desired allocation
+ 
+%Counts the total number of robots allocated to each task, then normalises
+%the results
 tmp = histcounts(A,'Normalization','probability');
 tmp = tmp*100;
-visE = tmp;
+ 
+%Finds the visual error between the bars (not necessarily equal to
+%MAE)
+visE = abs(tmp(1)-tmp(2))/2;
+ 
+%X axis for the bar chart 
 x = 1:length(tmp);
-
+ 
+%Y axis for the bar chart
 for i =1:length(tmp)
     y(i,1) = tmp(i);
     y(i,2) = 100*(qualities(i)/ sum(qualities));
 end
-
+ 
+% Showing the bar chart
 figure(2);
 tmp = bar(x,y,0.75);
-tmp(1).FaceColor = [0 1 1];
-tmp(2).FaceColor = [0 1 0];
+ 
+%Sets the colour for the obtained distribution (cyan)
+tmp(1).FaceColor = [0 1 1]; 
+ 
+%Sets the colour for the obtained distribution (green)
+tmp(2).FaceColor = [0 1 0]; 
+ 
+%Sets up the legend labels for the bar chart
 set(tmp, {'DisplayName'}, {'Obtained','Expected'}');
+ 
+%Axes labels for the bar chart
 xlabel('Target');
 ylabel('Number of robots (%)');
 ylim([0 70]);
-
+ 
+% Showing the legend for the bar chart
 legend('Location','northwest')
-%% Error Max and average
-
+ 
+%% Calculation of results
+ 
+%Calculating the maximum and average MAE
 maxE = max(mae);
 avgE = mean(mae);
-visE = abs(visE(1)-visE(2))/2;
-
-%% Output of results
-format short
+ 
+%Displaying the average and maximum mae alongside total distance
+%Note that mae is shown in % and distance is shown in m.
 disp('   AvgE(%)   MaxE(%)   Avg Dist');
-% disp('    AvgE      MaxE');
 disp([100*avgE 100*maxE (mean(dists)/10)]);
-
-out = [floor(A),mae',dists'];
-%% Writing results to csv for validation
-writematrix((out),'Test.csv');
-
+ 
+% Writing results to csv for validation
+% out = [floor(A),mae',dists'];
+% writematrix((out),'Test.csv');
